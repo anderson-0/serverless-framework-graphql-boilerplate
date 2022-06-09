f065de4af225d7230651be8ded280294de7cb911
Anderson Silva <anderson.consultant.it@outlook.com>
2022-06-09 19:30:17 -0400

    feat: defined apollo server with typedefs and resolvers

--- src/graphql.ts
+++ src/graphql.ts
@@ -4,6 +4,18 @@ import { ApolloServer, gql } from 'apollo-server-lambda';
 // get the GraphQL schema
 const schema = fs.readFileSync('./src/schema.graphql', 'utf8')
 
-exports.handler = () => {
-  console.log('Hello GraphQL');
-}
+// resolver functions
+const resolvers = { 
+  Query: {
+    item: () => {},
+  },
+
+  Mutation: {
+    createItem: () => {},
+  }
+};
+
+const server = new ApolloServer({ typeDefs: schema, resolvers })
+
+// launch the server when the Lambda is called
+exports.handler = server.createHandler();
