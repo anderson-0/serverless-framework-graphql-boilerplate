a55ab3b82d02cac60f247cd20c91524d5d7b49c7
Anderson Silva <anderson.consultant.it@outlook.com>
2022-06-09 19:29:21 -0400

    feat: defined initialschema

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,3 +1,9 @@
+import fs from 'fs';
+import { ApolloServer, gql } from 'apollo-server-lambda';
+
+// get the GraphQL schema
+const schema = fs.readFileSync('./src/schema.graphql', 'utf8')
+
 exports.handler = () => {
   console.log('Hello GraphQL');
 }
