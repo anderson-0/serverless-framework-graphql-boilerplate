00b7bbe0ae07ce18bd72b9b04e6293f2cc54d28c
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2022-06-17 08:14:31 -0400

    chore: fixed dependencies and added graphql playground

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,11 +1,22 @@
 import fs from 'fs';
 import { ApolloServer } from 'apollo-server-lambda';
 import resolvers from './graphql/resolvers';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
 // get the GraphQL schema
 const typeDefs = fs.readFileSync('./src/graphql/schemas/schema.graphql', 'utf8')
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const plugins = []
+if (process.env.NODE_ENV === 'development') {
+  plugins.push(ApolloServerPluginLandingPageGraphQLPlayground)
+}
+
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers, 
+  csrfPrevention: true,
+  plugins 
+});
 
 // launch the server when the Lambda is called
 exports.handler = server.createHandler();
